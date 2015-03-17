1ca09bd3d3faea616c1ffe8715cc718c19b2119b
Jonathan Warner <jaxbot@gmail.com>
2015-03-16 20:03:28 -0400

    Initial Commit

---
+++ SlackPack/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SlackPack
+//
+//  Created by Jonathan Warner on 3/16/15.
+//  Copyright (c) 2015 Jonathan Warner. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
