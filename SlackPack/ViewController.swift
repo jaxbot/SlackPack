fbcd4576e16cbcb24bf64dd3163b2b556787e2e1
Jonathan Warner <jaxbot@gmail.com>
2015-03-16 20:53:20 -0400

    Copy over basic webview code from my ADB project

---
+++ SlackPack/ViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  ADB Browser OSX
+//
+//  Copyright (c) 2015 Jonathan Warner. All rights reserved.
+//
+
+import Cocoa
+import WebKit
+import Foundation
+
+class ViewController: NSViewController, WKScriptMessageHandler {
+    var webView: WKWebView?
+    var localBase = NSURL(string: "about:blank")
+    
+    override func loadView() {
+        var contentController = WKUserContentController();
+        contentController.addScriptMessageHandler(
+            self,
+            name: "callbackHandler"
+        )
+
+        var config = WKWebViewConfiguration()
+        config.userContentController = contentController
+        self.webView = WKWebView(frame: NSRect(x: 0,y: 0,width: 0,height: 0), configuration: config)
+        
+        self.view = self.webView!
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var url = NSURL(string: "https://orlandodevs.slack.com/");
+        var request = NSURLRequest(URL: url!)
+        webView?.loadRequest(request)
+        var cwdString = "window.cwd=\"" + (NSProcessInfo.processInfo().arguments[0] as String) + "\""
+        webView?.evaluateJavaScript(cwdString, completionHandler: nil)
+    }
+    
+    func userContentController(userContentController: WKUserContentController,didReceiveScriptMessage message: WKScriptMessage) {
+        if(message.name == "callbackHandler") {
+            var data = message.body as NSDictionary
+            
+            var string = getStringFromCommand(data["command"] as String, arguments: data["arguments"] as [String])
+            string = (string.stringByReplacingOccurrencesOfString("\n", withString: "\\n", options: nil, range: nil))
+            string = (string.stringByReplacingOccurrencesOfString("\r", withString: "\\r", options: nil, range: nil))
+            var callbackString = "window.callbacksFromOS[\"" + (data["callbackFunction"] as String) + "\"](\"" + string + "\")"
+            webView?.evaluateJavaScript(callbackString, completionHandler: nil)
+        }
+    }
+    
+    func getStringFromCommand(command: String, arguments: [String]) -> String {
+        let task = NSTask()
+        task.launchPath = command
+        task.arguments = arguments
+        
+        let pipe = NSPipe()
+        task.standardOutput = pipe
+        task.launch()
+        
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let output: String = NSString(data: data, encoding: NSUTF8StringEncoding)!
+        
+        return output
+    }
+    
+    override var representedObject: AnyObject? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+}
