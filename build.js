b25dab5eef3a49ce7f80618b5f5519b59325e857
Jonathan Warner <jaxbot@gmail.com>
2015-03-18 21:44:59 -0400

    Build CSS files

--- build.js
+++ build.js
@@ -1,15 +1,24 @@
 var fs = require('fs');
 
-var htmlFiles = fs.readdirSync("html");
-var htmlObj = {};
-
-for (var i = 0; i < htmlFiles.length; i++) {
-    var contents = fs.readFileSync("html/" + htmlFiles[i]);
-    htmlObj[htmlFiles[i].replace(".html", "")] = contents.toString();
-}
-
-var build = "window.HTML = " + JSON.stringify(htmlObj) + ";\n";
+var build = "window.HTML = " + JSON.stringify(dirToObject("html")) + ";\n";
+build += "window.CSS = " + JSON.stringify(dirToObject("css")) + ";\n";
 build += fs.readFileSync("extension.js").toString();
 
 fs.writeFileSync("build/build.js", build);
+
+function dirToObject(dir) {
+    var dirFiles = fs.readdirSync(dir);
+    var dirObj = {};
+
+    for (var i = 0; i < dirFiles.length; i++) {
+        var contents = fs.readFileSync(dir + "/" + dirFiles[i]);
+        var filename = dirFiles[i].split(".");
+        filename.pop();
+        filename.join(".");
+
+        dirObj[filename] = contents.toString();
+    }
+
+    return dirObj;
+}
 
